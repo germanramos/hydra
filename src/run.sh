1cbc2bdb0d47146ed58585cb7cbb864aef65b34c
apenedo <icedfiend@gmail.com>
2013-06-18 13:21:01 +0200

    changed node routes and added 3rd parameter for app_manager_info_server port

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 killall -9 node
 killall -9 python
-node ./client_api/app/main.js --port=$2 --env=$1 &
-python ./app_manager/app_manager_info_server.py localhost $2 0.0.0.0 7777 $! &
-node ./server_api/app/main.js --port=$3 --env=$1 &
+/usr/local/bin/node ./client_api/app/main.js --port=$2 --env=$1 &
+python ./app_manager/app_manager_info_server.py localhost $2 0.0.0.0 $4 $! &
+/usr/local/bin/node ./server_api/app/main.js --port=$3 --env=$1 &
 cd ./app_manager
 python ./app_manager.py &
 cd ..
