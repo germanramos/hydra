dc68de45df8887b2a1ae9e54d96a2384f667434b
apenedo <icedfiend@gmail.com>
2013-08-28 12:11:11 +0200

    cleaned up unused variables and configurations, removed monitor since it's no longer needed

--- src/server_api/app/hydra_sync.js
+++ src/server_api/app/hydra_sync.js
@@ -48,7 +48,8 @@ module.exports = new function (){
 		}
 
 		for ( var f=0, F=_Servers.length; f<F;  f++ ) {
-			if(_Servers[f].server.indexOf(_config.url) > -1) continue; //ignoring self on sync servers
+			//ignoring self on sync servers
+			if(_Servers[f].server.indexOf(_config.publicUrl) > -1) continue;
 			var urlParts = utils.splitUrl(_Servers[f].server);
 			var url = urlParts.protocol + '://' + urlParts.host + ':' + _config.serverPort;
 			_syncServer(url);
