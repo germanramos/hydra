f131830a62baa9944d0e37ef23e074442a0aadb2
germanramos <german.ramos@gmail.com>
2013-09-30 18:33:43 +0200

    Integrated with qlog

--- src/app_manager/redisInfoProbe.py
+++ src/app_manager/redisInfoProbe.py
@@ -10,10 +10,18 @@ import time
 import json
 import redis
 import threading
+import urllib2
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 REDIS_KEY = "libertyStatusPing"
+
+QLOG_CLIENT_ID = "grAUUne3Lv1eqYW5"
+QLOG_SECRET_KEY = "F37P1Y9Xpr8TkltjyDHUsvIN2S48htfu"
+QLOG_URL = "1.qlog.innotechapp.com"
+QLOG_PORT = 3001
+
+#TODO: QLOG parameters
 
 class Listener(threading.Thread):
     def __init__(self, r, channels):
@@ -27,14 +35,28 @@ class Listener(threading.Thread):
             print "init", str(e)
     def work(self, item):
         try:
-            print item['channel'], ":", item['data']
-            data = {
-                    "cpuLoad": psutil.cpu_percent(interval=0.1, percpu=False),
-                    "memLoad": psutil.virtual_memory().percent
-            }
-            msg = json.dumps(data);
-            #TODO: Post to QLOG insted of print
-            print msg
+            if int(item['data']) > 1:
+                print item['channel'], ":", item['data']
+                systemStatus = {
+                                "cpuLoad": psutil.cpu_percent(interval=0.1, percpu=False),
+                                "memLoad": psutil.virtual_memory().percent
+                }
+                data = {
+                        "msg": json.dumps(systemStatus),
+                        "tags": "Probe",
+                        "time": item['data'],
+                        "secretKey": QLOG_SECRET_KEY
+                }
+                msg = json.dumps(data);
+                url = "http://" + QLOG_URL + ":" + str(QLOG_PORT) + "/app/" + QLOG_CLIENT_ID + "/log"
+                print "Posting data to " + url
+                print data
+                req = urllib2.Request(url, msg, {'Content-Type': 'application/json'})
+                req.get_method = lambda: 'PUT'
+                f = urllib2.urlopen(req)
+                response = f.read()
+                f.close()
+                print "Done OK"
         except Exception, e:
             print "work", str(e)
     
@@ -59,6 +81,7 @@ def reconnect():
     r = redis.Redis(REDIS_HOST, REDIS_PORT)
     client = Listener(r, [REDIS_KEY])
     client.start()
+    print "Connected"
 
 def sleepTillBreak():
     try:
