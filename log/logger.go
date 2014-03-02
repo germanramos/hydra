d9e338731e189539f613263e36dd4ca173cd6c2e
alberto <aguean.atbelor@gmail.com>
2014-03-02 14:28:01 +0100

    the first version of the logger has been completed, it's like etcd's

--- log/logger.go
+++ log/logger.go
@@ -1,8 +1,48 @@
 package log
 
 import (
+	"os"
+
 	golog "github.com/innotech/hydra/vendors/github.com/coreos/go-log/log"
 )
 
-var logger *golog.Logger := log.New("hydra", false,
-		log.CombinedSink(os.Stdout, "[%s] %s %-9s | %s\n", []string{"prefix", "time", "priority", "message"}))
+var Verbose bool = false
+
+var logger *golog.Logger = golog.New("hydra", false,
+	golog.CombinedSink(os.Stdout, "[%s] %s %-9s | %s\n", []string{"prefix", "time", "priority", "message"}))
+
+func Infof(format string, v ...interface{}) {
+	logger.Infof(format, v...)
+}
+
+func Info(v ...interface{}) {
+	logger.Info(v...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	if Verbose {
+		logger.Debugf(format, v...)
+	}
+}
+
+func Debug(v ...interface{}) {
+	if Verbose {
+		logger.Debug(v...)
+	}
+}
+
+func Warnf(format string, v ...interface{}) {
+	logger.Warningf(format, v...)
+}
+
+func Warn(v ...interface{}) {
+	logger.Warning(v...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	logger.Fatalf(format, v...)
+}
+
+func Fatal(v ...interface{}) {
+	logger.Fatalln(v...)
+}
