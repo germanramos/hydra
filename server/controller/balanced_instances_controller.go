eba0302e186e627caaf88e61af89347042265182
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-03-27 10:04:11 +0100

    Fixed bugs in balanced_instances_controller

--- server/controller/balanced_instances_controller.go
+++ server/controller/balanced_instances_controller.go
@@ -1,103 +1,82 @@
 package controller
 
 import (
-	// "encoding/json"
-	// "net/http"
+	"encoding/json"
+	"net/http"
 
-	// zmq "github.com/innotech/hydra/vendors/github.com/alecthomas/gozmq"
-	"github.com/innotech/hydra/vendors/github.com/gorilla/mux"
-	// uuid "github.com/innotech/hydra/vendors/github.com/nu7hatch/gouuid"
-
-	// "github.com/innotech/hydra/load_balancer"
-	// . "github.com/innotech/hydra/model/entity"
+	. "github.com/innotech/hydra/load_balancer"
+	. "github.com/innotech/hydra/model/entity"
 	. "github.com/innotech/hydra/model/repository"
+	"github.com/innotech/hydra/vendors/github.com/gorilla/mux"
 )
 
-var ZMQ_EMPTY_PART = []byte("")
+// var ZMQ_EMPTY_PART = []byte("")
 
 type BalancedInstancesController struct {
 	*BasicController
+	loadBalancerAddress string
 }
 
-func NewBalancedInstancesController() (*BalancedInstancesController, error) {
+func NewBalancedInstancesController(loadBalancerAddresss string) (*BalancedInstancesController, error) {
 	var b = new(BalancedInstancesController)
-	b.basePath = "/applications"
+	b.basePath = "/apps"
+	b.loadBalancerAddress = loadBalancerAddresss
 	var err error
 	b.PathVariables, err = extractPathVariables(b.basePath)
 	if err != nil {
 		return nil, err
 	}
 	b.repo = NewEctdRepository()
-	// TODO: fixed routes to constants
-	b.repo.SetCollection("/applications")
+	b.repo.SetCollection("/apps")
 	return b, nil
 }
 
-// func (b *BalancedInstancesController) sendZMQRequestToBalancer(app []byte, data [][]byte) (reply [][]byte) {
-// 	// TODO: Load Balancer from config
-// 	client := NewClient("tcp://localhost:5555" /*, verbose*/)
-// 	defer client.Close()
+func (b *BalancedInstancesController) sendZMQRequestToBalancer(app []byte, data [][]byte) (reply [][]byte) {
+	client := NewClient(b.loadBalancerAddress)
+	defer client.Close()
 
-// 	reply = client.Send(app, data)
-// 	return
-
-// 	// context, _ := zmq.NewContext()
-// 	// defer context.Close()
-
-// 	// // Set unique identity to make tracing possible
-// 	// identityUUID, _ := uuid.NewV4()
-// 	// identity := identityUUID.String()
-
-// 	// client, _ := context.NewSocket(zmq.REQ)
-// 	// client.SetIdentity(identity)
-// 	// // TODO: Make a constant address
-// 	// client.Connect("ipc://frontend.ipc")
-// 	// defer client.Close()
-
-// 	// // Send request, get reply
-// 	// client.SendMultipart([][]byte{[]byte(identity), ZMQ_EMPTY_PART, jsonApp}, 0)
-// 	// reply, _ := client.Recv(0)
-// 	// return reply
-// }
+	reply = client.Send(app, data)
+	return
+}
 
 func (b *BalancedInstancesController) RegisterHandlers(r *mux.Router) {
 	r.HandleFunc("/applications/{appId}/instances", b.List).Methods("GET")
 }
 
-// func (b *BalancedInstancesController) List(rw http.ResponseWriter, req *http.Request) {
-// 	vars := mux.Vars(req)
-// 	appId := vars["appId"]
-// 	app, err := b.repo.Get(appId)
-// 	if err != nil {
-// 		http.Error(rw, err.Error(), http.StatusNotFound)
-// 		return
-// 	}
-// 	appEntity, err := NewApplicationFromEtcdBaseModel(app)
-// 	if err != nil {
-// 		http.Error(rw, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// 	balancers, err := json.Marshal(appEntity.Balancers)
-// 	if err != nil {
-// 		http.Error(rw, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// 	instances, err := json.Marshal(appEntity.Instances)
-// 	if err != nil {
-// 		http.Error(rw, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
+func (b *BalancedInstancesController) List(rw http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	appId := vars["appId"]
+	app, err := b.repo.Get(appId)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusNotFound)
+		return
+	}
+	appEntity, err := NewApplicationFromEtcdBaseModel(app)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	balancers, err := json.Marshal(appEntity.Balancers)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	instances, err := json.Marshal(appEntity.Instances)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-// 	// Request to balancer server
-// 	// response := b.sendZMQRequestToBalancer([]byte(appEntity.Id), [][]byte{balancers, instances})
-// 	// TODO: process response
+	// Request to balancer server
+	response := b.sendZMQRequestToBalancer([]byte(appEntity.Id), [][]byte{balancers, instances})
+	// TODO: process response
 
-// 	jsonOutput, err := json.Marshal(response)
-// 	if err != nil {
-// 		http.Error(rw, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// 	rw.WriteHeader(http.StatusOK)
-// 	rw.Header().Set("Content-Type", "application/json")
-// 	rw.Write(jsonOutput)
-// }
+	jsonOutput, err := json.Marshal(response)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(jsonOutput)
+}
