0c130d92f7bafbbd56c43a166834bad4f4e25b95
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-04-02 16:06:16 +0200

    Fixed bugs in tests

--- tests/helpers/hydra_server.go
+++ tests/helpers/hydra_server.go
@@ -5,11 +5,16 @@ import (
 )
 
 // const PRIVATE_HYDRA_URI string = "127.0.0.1:8082"
+const DATA_DIR_PATH = "/tmp/node0"
 
 func RunHydraInStandaloneAndReturnProcess(privateAddr string) *os.Process {
 	procAttr := new(os.ProcAttr)
 	procAttr.Files = []*os.File{nil, os.Stdout, os.Stderr}
-	args := []string{"hydra", "-f", "-name=node0", "-private-addr=" + privateAddr, "-data-dir=/tmp/node0 -apps_file=fixtures/apps.empty.json"}
+	dataDirExits, err := existsPath(DATA_DIR_PATH)
+	if err == nil && dataDirExits {
+		os.RemoveAll(DATA_DIR_PATH)
+	}
+	args := []string{"hydra", "-f", "-name=node0", "-private-addr=" + privateAddr, "-data-dir=" + DATA_DIR_PATH, "-apps-file=fixtures/apps.empty.json"}
 
 	process, err := os.StartProcess(HydraBinPath, args, procAttr)
 	if err != nil {
@@ -21,4 +26,15 @@ func RunHydraInStandaloneAndReturnProcess(privateAddr string) *os.Process {
 
 func KillHydraProcess(process *os.Process) {
 	process.Kill()
+}
+
+func existsPath(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
