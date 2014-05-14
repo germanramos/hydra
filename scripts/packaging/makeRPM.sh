ec24420fe7dc02b13d2267a515b9d141d6f0a542
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-05-14 15:15:21 +0200

    Fixed bug in RPM script

--- scripts/packaging/makeRPM.sh
+++ scripts/packaging/makeRPM.sh
@@ -6,17 +6,17 @@ sudo yum install rpm-build rpmdevtools
 rm -rf ~/rpmbuild
 rpmdev-setuptree
 
-mkdir ~/rpmbuild/SOURCES/hydra
-cp ./fixtures/hydra.conf  ~/rpmbuild/SOURCES/hydra
-cp ./fixtures/apps-example.json  ~/rpmbuild/SOURCES/hydra
-cp hydra-init.d.sh ~/rpmbuild/SOURCES/hydra
+mkdir ~/rpmbuild/SOURCES/hydra-3
+cp ./fixtures/hydra.conf  ~/rpmbuild/SOURCES/hydra-3
+cp ./fixtures/apps-example.json  ~/rpmbuild/SOURCES/hydra-3
+cp hydra-init.d.sh ~/rpmbuild/SOURCES/hydra-3
 
 cp hydra.spec ~/rpmbuild/SPECS
 
 pushd ~/rpmbuild/SOURCES/
-tar czf hydra.tar.gz hydra/
+tar czf hydra-3.0.tar.gz hydra/
 cd ~/rpmbuild 
 rpmbuild -ba SPECS/hydra.spec
 
 popd
-cp ~/rpmbuild/RPMS/x86_64/hydra.x86_64.rpm .
+cp ~/rpmbuild/RPMS/x86_64/hydra-3-0.x86_64.rpm .
